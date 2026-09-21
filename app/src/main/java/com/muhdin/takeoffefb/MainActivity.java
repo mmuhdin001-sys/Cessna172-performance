1a42973365a7c43bb795ca5fa487a0155a87c79c
mmuhdin001-sys <mmuhdin001@gmail.com>
2026-09-21 12:11:57 +0300

    Refactor WebView initialization and set background color

--- app/src/main/java/com/muhdin/takeoffefb/MainActivity.java
+++ app/src/main/java/com/muhdin/takeoffefb/MainActivity.java
@@ -4,18 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.graphics.Color;
 
 public class MainActivity extends Activity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView webView = new WebView(this);
+        webView = new WebView(this);
 
+        webView.setBackgroundColor(Color.WHITE);
         webView.setWebViewClient(new WebViewClient());
 
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
 
         webView.loadUrl("file:///android_asset/index.html");
 
