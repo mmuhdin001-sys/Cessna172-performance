3edfd3c37c354eef17094790d2adf68a64ecff34
mmuhdin001-sys <mmuhdin001@gmail.com>
2026-09-21 11:03:26 +0300

    Add WebView to MainActivity for web content display

--- app/java/com/muhdin/takeoffefb/MainActivity.java
+++ app/java/com/muhdin/takeoffefb/MainActivity.java
@@ -1,1 +1,20 @@
 
+package com.muhdin.takeoffefb;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+
+        setContentView(webView);
+    }
+}
