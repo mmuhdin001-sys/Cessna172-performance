09c2a1fcc0949ea3a02a4265b46ed694918b46ff
mmuhdin001-sys <mmuhdin001@gmail.com>
2026-09-21 12:21:29 +0300

    Implement error handling in MainActivity
    Added error handling for WebView initialization.

--- app/src/main/java/com/muhdin/takeoffefb/MainActivity.java
+++ app/src/main/java/com/muhdin/takeoffefb/MainActivity.java
@@ -4,26 +4,32 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 import android.graphics.Color;
 
 public class MainActivity extends Activity {
 
-    private WebView webView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        try {
+            super.onCreate(savedInstanceState);
 
-        webView = new WebView(this);
+            WebView webView = new WebView(this);
+            webView.setWebViewClient(new WebViewClient());
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setDomStorageEnabled(true);
 
-        webView.setBackgroundColor(Color.WHITE);
-        webView.setWebViewClient(new WebViewClient());
+            webView.loadUrl("file:///android_asset/index.html");
 
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
+            setContentView(webView);
 
-        webView.loadUrl("file:///android_asset/index.html");
-
-        setContentView(webView);
+        } catch (Exception e) {
+            TextView error = new TextView(this);
+            error.setTextColor(Color.RED);
+            error.setTextSize(16);
+            error.setPadding(30, 30, 30, 30);
+            error.setText("APP ERROR:\\n\\n" + e.toString());
+            setContentView(error);
+        }
     }
 }
